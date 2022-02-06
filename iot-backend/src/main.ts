c37fca7be59c389fd25063b08157bb3bc98fcd41
Yoppy <yoppy@Junyeobui-MacBookPro.local>
2022-02-06 13:46:14 +0900

    remove code

--- iot-backend/src/main.ts
+++ iot-backend/src/main.ts
@@ -8,7 +8,6 @@ process.env.TZ = 'Asia/Seoul';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:8081', 'http://127.0.0.1:8081'],
